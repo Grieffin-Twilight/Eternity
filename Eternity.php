25a9ac1fae055834c0ef3bbf5ef49caa79f6a4d3
Grieffin-Twilight <97674340+Grieffin-Twilight@users.noreply.github.com>
2022-01-21 09:00:46 +0800

    Create Eternity.php

---
+++ Eternity.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Eternity</title>
+<Style>
+img.Raiden {width: 400px;
+    height: 400px ; 
+Border-radius: 100%;
+float: left ;
+margin-right: 50px ;
+object-fit: cover;
+}
+Img.Kazuha {Width: 400px;
+    height: 400px ;
+Border-radius:100%;
+Float: right;
+Margin-right: 50px;
+object-fit: cover;}
+Td{font-size:30px;
+ } 
+
+
+</Style>
+</head>
+<body bgcolor="#8878c3">
+<table>
+    <tr> 
+    <td>"You're temporary, i'm eternity <br> we're not the same" </br> </td> 
+    <td>But he's just so DAMN CUTE!<br> I mean LOOK AT THAT SMILE! </br> </td>
+    </tr>
+
+     
+    <td> <img class="Raiden"; Img src="/Images/Raiden Shogun.jpg"> </td>
+    <td><img class="Kazuha"; Img src="/images/Cute.jpg"> </td>
+</tr>
+</img>
+</table>
+</body>    
+ 
+</html>
